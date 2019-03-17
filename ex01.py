85c5bc3ad6cce2a442420f732524d05c21b51619
bnpysse <bnpysse@aliyun.com>
2019-03-17 13:44:37 +0800

    Change the hello world

--- ex01.py
+++ ex01.py
@@ -1,2 +1,9 @@
 for i in range(10):
     print('{} Hello World!'.format(i))
+
+class Sample_Object:
+    pass
+
+if __name__ == "__main__":
+    sample_object = Sample_Object()
+    print(sample_object.__dir__())
